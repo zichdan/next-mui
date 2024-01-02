bf3c8c5998b1faa22b0c80b6c6c656f67ba733a2
zichdan <zichdan1999@gmail.com>
2024-01-02 03:34:48 +0100

    Dashboard refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { Inter } from 'next/font/google'
+import Dashboard from './dashboard/Dashboard'
 
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
-
+      <Dashboard/>
     </main>
   )
 }
