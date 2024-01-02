ef9da8018f341cd17c60b2c605de2756373b289c
zichdan <zichdan1999@gmail.com>
2024-01-02 04:09:29 +0100

    Added data, profile, settings to dashboard components

--- src/pages/components/SideMenu/SideMenu.tsx
+++ src/pages/components/SideMenu/SideMenu.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const SideMenu = () => {
   return (
-    <div>SideMenu</div>
+        <ul>
+            <li>Analytics</li>
+            <li>Profile</li>
+            <li>Settings</li>
+        </ul>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import { Inter } from 'next/font/google'
+import Header from './components/Header'
+import SideMenu from './components/SideMenu'
 import Dashboard from './dashboard/Dashboard'
-
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
-      <Dashboard/>
+      <Header/>
+      <SideMenu/>
+      <Dashboard/>  
     </main>
   )
 }
